9cad513443c695842f466672ee27c11127e4fdb9
f4anks <frankhsistemas@gmail.com>
2026-02-10 12:05:00 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,23 @@
-const CACHE_NAME = 'control-voley-v1';
+const CACHE_NAME = 'cv-cache-v1';
 const ASSETS = [
   './',
   './index.html',
-  './index_menu.html',
-  './index_atletas.html',
-  './index_datos.html',
-  './index_ficha.html',
-  './index_cumple.html',
-  './index_estad.html',
-  './img/banner.png'
+  './manifest.json',
+  './libs/tailwind.js',
+  './libs/charts.js',
+  './img/logo.png'
 ];
 
-// Instalación: Guardar archivos en caché
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-// Estrategia de carga: Primero caché, luego red
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
